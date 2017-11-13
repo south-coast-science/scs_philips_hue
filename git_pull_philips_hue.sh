c421c973499328c9fe047bb406b138f4bc0d93e4
Bruno Beloff <bruno.beloff@southcoastscience.com>
2017-11-13 17:51:46 +0000

    Added git_pull_philips_hue.sh

---
+++ git_pull_philips_hue.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+GIT_PATH=~/SCS/scs_analysis/
+echo ${GIT_PATH}
+git -C ${GIT_PATH} pull
+echo '-'
+
+GIT_PATH=~/SCS/scs_core/
+echo ${GIT_PATH}
+git -C ${GIT_PATH} pull
+echo '-'
+
+GIT_PATH=~/SCS/scs_host_posix/
+echo ${GIT_PATH}
+git -C ${GIT_PATH} pull
+echo '-'
+
+GIT_PATH=~/SCS/scs_osio/
+echo ${GIT_PATH}
+git -C ${GIT_PATH} pull
+echo '-'
+
+GIT_PATH=~/SCS/scs_philips_hue/
+echo ${GIT_PATH}
+git -C ${GIT_PATH} pull
+echo '-'
