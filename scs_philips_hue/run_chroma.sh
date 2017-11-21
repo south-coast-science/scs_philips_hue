aad55c9c09d9a9d2e1838346646d4cc0fb3431df
Bruno Beloff <bruno.beloff@southcoastscience.com>
2017-11-21 03:51:20 +0000

    Added run_chroma.sh

--- scs_philips_hue/run_chroma.sh
+++ scs_philips_hue/run_chroma.sh
@@ -5,4 +5,6 @@
 # $3: brightness
 # $4: optional -v
 
+# example: ./run_chroma.sh /orgs/south-coast-science-demo/brighton/loc/1/particulates val.pm2p5 128 &
+
 ./osio_mqtt_subscriber.py $4 $1 | ./node.py $4 $1.$2 | ./chroma.py $4 -d 0 50 -r G R -t 9.0 -b $3 | ./light.py $4 -r 1
