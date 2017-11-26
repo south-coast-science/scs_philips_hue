f18471fc1ceb2806908150699143030ddfeaa8ce
Bruno Beloff <bruno.beloff@southcoastscience.com>
2017-11-26 09:37:45 +0000

    Fixed run_chroma.sh

--- scs_philips_hue/run_chroma.sh
+++ scs_philips_hue/run_chroma.sh
@@ -2,10 +2,11 @@
 
 # $1: topic path
 # $2: document node
-# $3: brightness
-# $4: lamp name
-# $5: optional -v
+# $3: domain max
+# $4: brightness
+# $5: lamp name
+# $6: optional -v
 
-# example: ./run_chroma.sh /orgs/south-coast-science-demo/brighton/loc/1/particulates val.pm2p5 128 scs-hcl-001 &
+# example: ./run_chroma.sh /orgs/south-coast-science-demo/brighton/loc/1/particulates val.pm2p5 50 128 scs-hcl-001 &
 
-./osio_mqtt_subscriber.py $5 $1 | ./node.py $5 $1.$2 | ./chroma.py $5 -d 0 50 -r G R -t 9.0 -b $3 | ./desk.py $5 $4
+./osio_mqtt_subscriber.py $6 $1 | ./node.py $5 $1.$2 | ./chroma.py $6 -d 0 $3 -r G R -t 9.0 -b $4 | ./desk.py $6 $5
