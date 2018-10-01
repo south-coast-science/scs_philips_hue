d8a981078e3044804e3665cf3c71b879bfcc99f8
Bruno Beloff <bruno.beloff@southcoastscience.com>
2018-10-01 15:14:46 +0100

    Added file specification for desk.

--- src/scs_philips_hue/cmd/cmd_chroma.py
+++ src/scs_philips_hue/cmd/cmd_chroma.py
@@ -16,7 +16,7 @@ class CmdChroma(object):
         """
         Constructor
         """
-        self.__parser = optparse.OptionParser(usage="%prog [-v]", version="%prog 1.0")
+        self.__parser = optparse.OptionParser(usage="%prog [-f FILE] [-v]", version="%prog 1.0")
 
         # optional...
         self.__parser.add_option("--file", "-f", type="string", nargs=1, action="store", dest="file",
--- src/scs_philips_hue/desk.py
+++ src/scs_philips_hue/desk.py
@@ -74,7 +74,7 @@ if __name__ == '__main__':
         # resources...
 
         # DeskConf...
-        conf = DeskConf.load(Host)
+        conf = DeskConf.load_from_file(cmd.file) if cmd.file else DeskConf.load(Host)
 
         if conf is None:
             print("desk: DeskConf not available.", file=sys.stderr)
