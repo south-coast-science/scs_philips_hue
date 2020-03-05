6cf8c9d3174d668e7f2ed5ccf61e05bc1fb7b088
Bruno Beloff <bruno.beloff@southcoastscience.com>
2020-03-05 20:52:15 +0000

    Cleaning

--- git_pull.sh
+++ git_pull.sh
@@ -2,37 +2,7 @@
 
 # copy this file to the ~/SCS directory and edit as required...
 
-GIT_PATH=~/SCS/scs_airnow/                  # comment out if the package is not installed
-echo ${GIT_PATH}
-git -C ${GIT_PATH} pull
-echo '-'
-
-GIT_PATH=~/SCS/scs_analysis/                # comment out if the package is not installed
-echo ${GIT_PATH}
-git -C ${GIT_PATH} pull
-echo '-'
-
-GIT_PATH=~/SCS/scs_comms_ge910/             # comment out if the package is not installed
-echo ${GIT_PATH}
-git -C ${GIT_PATH} pull
-echo '-'
-
-GIT_PATH=~/SCS/scs_core/                    # comment out if the package is not installed
-echo ${GIT_PATH}
-git -C ${GIT_PATH} pull
-echo '-'
-
-GIT_PATH=~/SCS/scs_dev/                     # comment out if the package is not installed
-echo ${GIT_PATH}
-git -C ${GIT_PATH} pull
-echo '-'
-
-GIT_PATH=~/SCS/scs_dfe_eng/                 # comment out if the package is not installed
-echo ${GIT_PATH}
-git -C ${GIT_PATH} pull
-echo '-'
-
-GIT_PATH=~/SCS/scs_display_inky/            # comment out if the package is not installed
+GIT_PATH=~/SCS/scs_core/
 echo ${GIT_PATH}
 git -C ${GIT_PATH} pull
 echo '-'
@@ -42,27 +12,7 @@ echo ${GIT_PATH}
 git -C ${GIT_PATH} pull
 echo '-'
 
-GIT_PATH=~/SCS/scs_mfr/                     # comment out if the package is not installed
-echo ${GIT_PATH}
-git -C ${GIT_PATH} pull
-echo '-'
-
-GIT_PATH=~/SCS/scs_ndir/                    # comment out if the package is not installed
-echo ${GIT_PATH}
-git -C ${GIT_PATH} pull
-echo '-'
-
-GIT_PATH=~/SCS/scs_osio/                    # comment out if the package is not installed
-echo ${GIT_PATH}
-git -C ${GIT_PATH} pull
-echo '-'
-
-GIT_PATH=~/SCS/scs_philips_hue/             # comment out if the package is not installed
-echo ${GIT_PATH}
-git -C ${GIT_PATH} pull
-echo '-'
-
-GIT_PATH=~/SCS/scs_psu/                     # comment out if the package is not installed
+GIT_PATH=~/SCS/scs_philips_hue/
 echo ${GIT_PATH}
 git -C ${GIT_PATH} pull
 echo '-'
