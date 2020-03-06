9116789881ada1772929675376207ecf6b36b179
Bruno Beloff <bruno.beloff@southcoastscience.com>
2020-03-06 09:51:59 +0000

    Added white LightState

--- src/scs_philips_hue/run_chroma.sh
+++ src/scs_philips_hue/run_chroma.sh
@@ -2,4 +2,6 @@
 
 # $1: optional -v
 
+# ./aws_mqtt_subscriber.py -v -c | ./node.py -v -c | ./chroma.py -v | ./desk.py -v
+
 ./aws_mqtt_subscriber.py $1 -c | ./node.py $1 -c | ./chroma.py $1 | ./desk.py $1
