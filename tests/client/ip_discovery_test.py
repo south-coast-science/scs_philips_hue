a5405aff054f9fddd4e65cc940c69fae9173810a
Bruno Beloff <bruno.beloff@southcoastscience.com>
2020-03-15 09:37:51 +0000

    Moved HTTPClient to scs_core

--- tests/client/ip_discovery_test.py
+++ tests/client/ip_discovery_test.py
@@ -8,7 +8,8 @@ Created on 4 Mar 2020
 
 import time
 
-from scs_host.client.http_client import HTTPClient
+from scs_core.client.http_client import HTTPClient
+
 from scs_host.sys.host import Host
 
 from scs_philips_hue.discovery.ip_discovery import IPDiscovery
