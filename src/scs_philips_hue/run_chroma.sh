4235b81bb743e9fbe393f54c3756a0d36c88100d
Bruno Beloff <bruno.beloff@southcoastscience.com>
2020-08-05 13:24:12 +0100

    Cleaned shell scripts

--- src/scs_philips_hue/run_chroma.sh
+++ src/scs_philips_hue/run_chroma.sh
@@ -4,4 +4,4 @@
 
 # ./aws_mqtt_subscriber.py -v -c | ./node.py -v -c | ./chroma.py -v | ./desk.py -v
 
-./aws_mqtt_subscriber.py $1 -e -c | ./node.py $1 -c | ./chroma.py $1 | ./desk.py $1
+./aws_mqtt_subscriber.py "$1" -e -c | ./node.py "$1" -c | ./chroma.py "$1" | ./desk.py "$1"
