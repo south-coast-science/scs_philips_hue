3c228faea1351a694a4b32b13e68d521084735d2
Bruno Beloff <bruno.beloff@southcoastscience.com>
2023-09-26 13:22:03 +0100

    Fixed pypi.sh for flexible 2FA token handling

--- pypi.sh
+++ pypi.sh
@@ -5,10 +5,11 @@
 # requires:
 # python -m pip install --upgrade build
 # python -m pip install --upgrade twine
+
 # https://pypi.org/manage/account/token/
 
 rm -rf dist
 python -m build
-twine upload dist/* $@
+twine upload dist/* "$@"
 
 rm -rf dist
